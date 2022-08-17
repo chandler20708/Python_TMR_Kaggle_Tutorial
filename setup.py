c2ebe25ebe9ecfbdbea9ce615a4bf236a3b6ed11
chandler20708 <74949771+chandler20708@users.noreply.github.com>
2022-08-17 18:53:13 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'Python_TMR_Kaggle_Tutorial',
+    version = '0.1.0',
+    url = '',
+    description = '',
+    packages = find_packages(),
+    install_requires = [
+        # Github Private Repository
+        'Python_TMR_Kaggle_Tutorial @ git+https://github.com/chandler20708/Python_TMR_Kaggle_Tutorial.git'
+    ]
+)
